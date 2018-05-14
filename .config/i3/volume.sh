f69c1485b090b04a2afecccc26cc5b9ff72b04a6
Louis Kraemer <louiskraemer@gmail.com>
2018-05-14 14:30:42 +0200

    updated volume icon

--- .config/i3/volume.sh
+++ .config/i3/volume.sh
@@ -5,53 +5,53 @@
 # $./volume.sh down
 # $./volume.sh mute
 
-function get_volume {
-    pamixer --get-volume
+function get_volume() {
+	pamixer --get-volume
 }
 
-function is_mute {
-    pamixer --get-mute
+function is_mute() {
+	pamixer --get-mute
 }
 
-function send_notification {
-    volume=`get_volume`
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-    if is_mute ; then
-        icon="audio-volume-muted-symbolic-osd"
-        bar=""
-    else
-        bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
-        case $volume in
-             0)
-                 icon="audio-volume-muted-symbolic-osd"
-                 ;;
-             [1-9]|[1-2][0-9]|30)
-                 icon="audio-volume-low-symbolic-osd" 
-                 ;;
-             31|[3-5][0-9]|60)
-                 icon="audio-volume-medium-symbolic-osd"
-                 ;;
-             61|[6-9][0-9]|100)
-                 icon="audio-volume-high-symbolic-osd"
-                 ;;
-        esac
-    fi
-    # Send the notification
-    dunstify -i $icon -r 9999 -t 1000 " $bar"
+function send_notification() {
+	volume=$(get_volume)
+	# Make the bar with the special character ─ (it's not dash -)
+	# https://en.wikipedia.org/wiki/Box-drawing_character
+	if is_mute; then
+		icon=$ICON/volume-mute.png
+		bar=""
+	else
+		bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
+		case $volume in
+		0)
+			icon=$ICON/volume-mute.png
+			;;
+		[1-9] | [1-2][0-9] | 30)
+			icon=$ICON/volume-off.png
+			;;
+		31 | [3-5][0-9] | 60)
+			icon=$ICON/volume-down.png
+			;;
+		61 | [6-9][0-9] | 100)
+			icon=$ICON/volume-up.png
+			;;
+		esac
+	fi
+	# Send the notification
+	dunstify -i $icon -r 9999 -t 1000 " $bar"
 }
 
 case $1 in
-    up)
+up)
 	pamixer -u -i 10
 	send_notification
 	;;
-    down)
+down)
 	pamixer -u -d 10
 	send_notification
 	;;
-    mute)
-    	pamixer -t
+mute)
+	pamixer -t
 	send_notification
 	;;
 esac
