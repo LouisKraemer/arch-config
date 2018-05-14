2c74207f5e9b686697b579379e19f3838f5e4470
Louis Kraemer <louiskraemer@gmail.com>
2018-05-14 14:22:33 +0200

    refactored brightness.sh

--- .config/i3/brightness.sh
+++ .config/i3/brightness.sh
@@ -5,25 +5,12 @@ function get_bright() {
 }
 
 function send_notification() {
-	#brightness=$(get_bright)
 	# Make the bar with the special character ─ (it's not dash -)
 	# https://en.wikipedia.org/wiki/Box-drawing_character
 	bar=$(seq -s "─" $(($newBrightness/5)) | sed 's/[0-9]//g')
 
-	case $roundBrightness in
-	[0-9] | [1-2][0-9] | 30)
-		icon=$ICON/brightness-low.png
-		;;
-	31 | [3-5][0-9] | 60)
-		icon=$ICON/brightness-low.png
-		;;
-	61 | [6-9][0-9] | 100)
-		icon=$ICON/brightness-low.png
-		;;
-	esac
-
 	# Send the notification
-	dunstify -I $icon -r 9999 -t 1500 " $bar"
+	dunstify -I $ICON/brightness.png -r 9999 -t 1500 " $bar"
 }
 
 brightness=$(get_bright)
